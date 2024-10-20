8c91b2e1d61df1bb25b6d4ba9ba242830e04cbbf
Richard-Carnivale <145724604+Richard-Carnivale@users.noreply.github.com>
2024-10-20 16:53:46 -0400

    fixed constant updating
    fixed the updating the control vertex after only being reselected, now it constantly reupdated as its being moved

--- operators/polyhedral_splines.py
+++ operators/polyhedral_splines.py
@@ -23,10 +23,9 @@ class PolyhedralSplines(bpy.types.Operator):
     bl_idname = "object.polyhedral_splines"
     bl_description = "Generates polyhedral spline mesh. Some mesh configurations are not supported, subdivide the mesh beforehand if this is the case"
     polyhedral_splines_finished = False
-    coverage_test_var = False
-    verts = {}
-    wireframe_vertices = []  # new
     patch_to_corners = {}
+    verts = []
+    full_verts = []
 
     def __init__(self):
         print("Start")
@@ -50,8 +49,8 @@ class PolyhedralSplines(bpy.types.Operator):
         self.__init_patch_obj__(context)
         bpy.ops.ui.reloadtranslation()
 
+        bpy.context.scene.polyhedral_splines_finished = True
         PolyhedralSplines.polyhedral_splines_finished = True
-        # bpy.context.scene.polyhedral_splines_finished = True
         return {'FINISHED'}
 
     def __init_patch_obj__(self, context):
@@ -64,7 +63,7 @@ class PolyhedralSplines(bpy.types.Operator):
         bm = bmesh.new()
         bm.from_mesh(control_mesh)
         bm.verts.ensure_lookup_table()
-        bm.faces.ensure_lookup_table()
+        bm.faces.ensure_lookup_table() 
 
         patchWrappers = PatchHelper.getPatches(bm)
         for patchWrapper in patchWrappers:
@@ -89,90 +88,77 @@ class PolyhedralSplines(bpy.types.Operator):
         else:
             bm.to_mesh(control_mesh)
             control_mesh.update()
-
+        
+        PolyhedralSplines.full_verts, PolyhedralSplines.verts = PolyhedralSplines.get_verts()
         bpy.app.handlers.depsgraph_update_post.append(edit_object_change_handler)
-
-
-    @staticmethod
-    def get_verts():
-        if input("full test? (y/n): ") != 'n':
-            for key, val in PolyhedralSplines.patch_to_corners.items():
-                # corner[1] is the corner coordinates because
-                # corner[0] is the type of contructed patch
-                coords = tuple(val[1])
-                tups.append((coords, key))
-            no_dupes_tups = []
-            seen_coords = set()
-            for coords, key in tups:
-
-                # -----------------------------------
-                # this block of code converts the coordinates into a tuple
-                # from a numpy array so it can be hashed and checked for duplicates
-                check_coords = []
-                for cord in coords:
-                    check_coords.extend(list(cord))
-                check_coords = tuple(check_coords)
-                # -----------------------------------
-
-                if check_coords not in seen_coords:
-                    no_dupes_tups.append((coords, key))
-                    seen_coords.add(check_coords)
+                
+    def get_verts(): 
+        # if input("full test? (y/n): ") != 'n':
+        for parent, val in PolyhedralSplines.patch_to_corners.items():
+            # corner[1] is the corner coordinates because
+            # corner[0] is the type of contructed patch
+            coords = tuple(val[1])
+            patch_type = val[0]
+            tups.append((coords, patch_type, parent))
+        no_dupes_tups = []
+        seen_coords = set()
+        for coords, patch_type, parent in tups:
 
             # -----------------------------------
-            # just to find the amount of control points
-            total_control_points = 0
-            unique_control_points = 0
-            for corners, constructor in tups:
-                for corner in corners:
-                    total_control_points += 1
-            for corners, constructor in no_dupes_tups:
-                for corner in corners:
-                    unique_control_points += 1
-            print(f"Number of total control points: {total_control_points}")
-            print(f"Number of unique control points: {unique_control_points}")
-        PolyhedralSplines.coverage_test_var = True
-        return no_dupes_tups
+            # this block of code converts the coordinates into a tuple
+            # from a numpy array so it can be hashed and checked for duplicates
+            check_coords = []
+            for cord in coords:
+                check_coords.extend(list(cord))
+            check_coords = tuple(check_coords)
+            # -----------------------------------
 
+            if check_coords not in seen_coords:
+                no_dupes_tups.append((coords, patch_type, parent))
+                seen_coords.add(check_coords)
+            
+        # -----------------------------------
+        # just to find the amount of control points
+        total_control_points = 0
+        unique_control_points = 0
+        for corners, constructor, parent in tups:
+            for corner in corners:
+                total_control_points += 1
+        for corners, constructor, parent in no_dupes_tups:
+            for corner in corners:
+                unique_control_points += 1
+        print(f"Number of total control points: {total_control_points}")
+        print(f"Number of unique control points: {unique_control_points}")
+        return tups, no_dupes_tups
 
-    @staticmethod
     def control_cube_test(patch_index_str):
         try:
             patch_index = int(patch_index_str)
             patch_name = "SurfPatch." + str(patch_index)
             # patch = bpy.context.scene.objects[patch_name]
             # add_control_cubes(patch) #TODO:
-            corners = PolyhedralSplines.verts[patch_name]
+            corners = PolyhedralSplines.patch_to_corners[patch_name]
             if corners != []:
                 for corner in corners:
                     coords = tuple(corner)
-                    PolyhedralSplines.create_control_cube(coords, bpy.context.scene.objects[patch_name])
+                    create_control_cube(coords, bpy.context.scene.objects[patch_name])
                 print("Control cubes added to patch", patch_index)
             else:
                 print("No corner coordinates found for patch", patch_index)
             # print("Control cubes added to patch", patch_index)
         except ValueError:
-            print("Invalid patch index. Please enter a number.")
+            print("Invalid patch index. Please enter a number.") 
 
-    @staticmethod
     def create_control_cube(location, parent_obj=None):
-        bpy.ops.mesh.primitive_cube_add(size=0.01, enter_editmode=False, align='WORLD', location=location,
-                                        scale=(1, 1, 1))
+        bpy.ops.mesh.primitive_cube_add(size=0.01, enter_editmode=False, align='WORLD', location=location, scale=(1, 1, 1))
         cube = bpy.context.active_object
         if parent_obj is not None:
             name = "ControlCube." + str(parent_obj.name)
             cube.name = name
-            cube.parent = parent_obj
+            cube.parent = parent_obj 
         else:
             name = "ControlCube."
             cube.name = name
-        #cube["vertex_indices"] = vertex_indices
-
-        PolyhedralSplines.wireframe_vertices.append(location)  # new
-
-        # new test - can probably remove these 2 lines
-        cube["original_location"] = location
-        cube["movement_vector"] = (0, 0, 0)
-
 
 # if previous mode is not edit, switching to edit has no need to update surface
 class Mode:
@@ -183,11 +169,9 @@ class Mode:
 # please see https://developer.blender.org/T73638
 prev_mode = 'OBJECT'
 
-
 @persistent
 def edit_object_change_handler(scene, context):
     obj = bpy.context.active_object
-    # print(obj.type)
 
     if scene.previous_object:
         prev_obj = scene.previous_object
@@ -200,36 +184,21 @@ def edit_object_change_handler(scene, context):
     if obj is None:
         return None
 
-    if bpy.context.scene.polyhedral_splines_finished:
-        if obj.type == 'SURFACE':
-            # print("PolyhedralSplines has finished, and a surface is selected")
-            if prev_obj != obj:
-                print("PolyhedralSplines has finished, and a new surface is selected")
-                # print("new")
-                # while input("Do you want to add control cubes to a patch? (y/n): ") != 'n':
-                # print(obj.name)
-                # Split the string by '.' and take the second part
-                patch_index_str = obj.name.split('.')[1]
-                PolyhedralSplines.control_cube_test(patch_index_str)
 
-    if obj.mode == 'EDIT' and Mode.prev == 'EDIT' and obj.type == 'MESH':
+    if obj.mode == 'EDIT' and Mode.prev == 'EDIT' and obj.type == 'MESH' and obj.name != "SurfaceMesh":
         update_surface(context, obj)
 
-    Mode.prev = obj.mode
 
-    # new test
-    if obj.type == 'MESH' and PolyhedralSplines.coverage_test_var and "ControlCube" in obj.name:
-        original_location = obj.get("original_location", None)
-        if original_location:
-            current_location = obj.location
-            movement_vector = (
-                current_location.x - original_location[0],
-                current_location.y - original_location[1],
-                current_location.z - original_location[2],
-            )
-            # print("Original Location:", original_location[0], original_location[1], original_location[2])
-            obj["movement_vector"] = movement_vector
-            print(f"{obj.name} moved: {movement_vector}")
+    '''
+    if obj.type == 'SURFACE':
+        print("You selected a surface")
+    if obj.type == 'MESH':
+        print("You selected a mesh")
+    print(obj.type, "inbetween")
+    print("in function")
+    '''
+
+    Mode.prev = obj.mode
 
     return None
 
@@ -282,5 +251,4 @@ def update_surface(context, obj):
 
 bpy.app.handlers.depsgraph_update_post.append(edit_object_change_handler)
 bpy.types.Scene.polyhedral_splines_finished = bpy.props.BoolProperty(default=False)
-bpy.types.Scene.coverage_test_var = bpy.props.BoolProperty(default=False)
 bpy.types.Scene.previous_object = bpy.props.PointerProperty(type=bpy.types.Object)
--- operators/surface_mesh.py
+++ operators/surface_mesh.py
@@ -1,28 +1,90 @@
 import bpy
 import bmesh
+from bpy.app.handlers import persistent
 from .polyhedral_splines import PolyhedralSplines
 
+import numpy as np
+from mathutils import Vector
+
+class PersistentState():
+    previous_mesh = None
+    previous_mesh_verts = None
+    previous_vertex_idx = None
+    previous_location = None
+    delta_location = (0, 0, 0)
+    selected = False
+    # delta_total = None
+# persitent_data = {}
 
 class SurfaceMesh(bpy.types.Operator):
+    # richard addition
     """fake net operator"""
     bl_label = "Surface Mesh"
     bl_idname = "object.create_surface_mesh"
     bl_description = "Creates a surface mesh"
+    control_mesh_name = None
     control_mesh_obj = None
-    control_mesh = None
     patch_to_corners = None
-    vertices = []
+    verts = None
+    full_verts = None
+    wireframe_mesh_created = False
+
 
     # poll function makes it so that you cannot select button function unless certain requirements are met
     # such as having an object selected
+    #richard addition
     @classmethod
     def poll(cls, context):
         obj = context.active_object
         selected = context.selected_objects
 
         if obj in selected and obj.mode == "OBJECT" and obj.type == "MESH" and PolyhedralSplines.polyhedral_splines_finished:
+            SurfaceMesh.control_mesh_obj = obj
+            SurfaceMesh.patch_to_corners = PolyhedralSplines.patch_to_corners
+            SurfaceMesh.verts = PolyhedralSplines.verts
+            SurfaceMesh.full_verts = PolyhedralSplines.full_verts
             return True
         return False
+
+    def distance(p1, p2):
+        return ((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2 + (p1[2] - p2[2]) ** 2) ** 0.5
+    
+    def delta_location(original_location, new_location):
+        return new_location - original_location
+
+    # Harry addition to test modifying mesh
+    def mesh_modification(vertex_index, delta_location):
+        control_mesh = bpy.data.objects[SurfaceMesh.control_mesh_name]
+
+        #if bpy.context.object.mode != 'OBJECT':
+        #    bpy.ops.object.mode_set(mode='OBJECT')
+
+        bm = bmesh.new()
+        bm.from_mesh(control_mesh.data)
+        bm.verts.ensure_lookup_table()
+
+        # undo the rotation from the original location
+        # original_location[1], original_location[2] = original_location[2], -original_location[1]
+
+        print("Before the locations are changed")
+        print(bm.verts[vertex_index].co)
+
+        if 0 <= vertex_index < len(bm.verts):
+            bm.verts[vertex_index].co += delta_location
+        else:
+            print(f"Invalid vertex_index: {vertex_index}")
+            return
+
+        print("After the locations are changed")
+        print("---------------------------------------------")
+
+        bm.to_mesh(control_mesh.data)
+        bm.free()
+
+        control_mesh.data.update()
+
+        #bpy.ops.object.mode_set(mode='EDIT')
+
 
     def execute(self, context):
         # Perform some action
@@ -32,56 +94,201 @@ class SurfaceMesh(bpy.types.Operator):
         print("Polyhedral Splines Finished:", PolyhedralSplines.polyhedral_splines_finished)
         if PolyhedralSplines.polyhedral_splines_finished:
             # fixed
-            PolyhedralSplines.get_verts()
-        self.create_wireframe_mesh(context)
+            # SurfaceMesh.mesh_modification()
+            # PolyhedralSplines.coverage_test()
+            self.create_wireframe_mesh(context)
         return {'FINISHED'}
 
     @staticmethod
     def create_wireframe_mesh(context):
+        mesh = bpy.data.meshes.new(name="WireframeMesh")
 
-        name = "WireframeObject"
-        mesh = bpy.data.meshes.new(name=name)
+        # Dictionary to store unique vertices
+        vert_dict = {}
+        verts = []
+        edges = set()
 
-        patch_to_corners = PolyhedralSplines.patch_to_corners
+        # Function to add a vertex and return its index
+        def add_vertex(vertex):
+            vertex_tuple = tuple(vertex)
+            if vertex_tuple not in vert_dict:
+                vert_dict[vertex_tuple] = len(verts)
+                verts.append(vertex_tuple)
+            return vert_dict[vertex_tuple]
 
-        vertices = []
-        edges = []
-        vert_index_map = {}
-
-        # Iterate over each patch and its vertices
-        for patch_name, (patch_type, corners) in patch_to_corners.items():
-            start_index = len(vertices)
-            # Add vertices for this patch
-            for corner in corners:
-                vertices.append(corner)
-                vert_index_map[tuple(corner)] = len(vertices) - 1
-
-            # Create edges for this patch
-            for i in range(len(corners)):
-                for j in range(i + 1, len(corners)):
-                    # Add edge only if not already added
-                    edge = (start_index + i, start_index + j)
-                    if edge not in edges and (edge[1], edge[0]) not in edges:
-                        edges.append(edge)
+        # Process regular faces first
+        face_verts = []
+        for face, face_type, parent in SurfaceMesh.full_verts:
+            if face_type == "Regular":
+                # Rotate vertex: [x, y, z] -> [x, -z, y]
+                for vert in face:
+                    rotated_vertex = np.array([vert[0], -vert[2], vert[1]])
+                    # rotated_vertex = np.array([vert[0], vert[1], vert[2]])
+                    # rotated_vertex = np.array([rotated_vertex[0], rotated_vertex[2], -rotated_vertex[1]])
+                    # Add vertex and get its index
+                    # rotated_vertex = np.array([vert[0], -vert[1], -vert[2]])
+                    vert_index = add_vertex(rotated_vertex)
+                    face_verts.append(vert_index)
+                
+                # If we have collected 4 vertices for a face
+                if len(face_verts) == 4:
+                    # Add edges for the face
+                    edges.add((face_verts[0], face_verts[1]))
+                    edges.add((face_verts[0], face_verts[2]))
+                    edges.add((face_verts[1], face_verts[3]))
+                    edges.add((face_verts[2], face_verts[3]))
+                    # Reset face_verts for the next face
+                    face_verts = [] 
+        
+        edges_list = list(edges)
 
         # Create mesh data
-        mesh.from_pydata(vertices, edges, [])
+        mesh.from_pydata(verts, edges_list, [])
+
+        # Update mesh
         mesh.update()
 
-        obj = bpy.data.objects.new(name=name, object_data=mesh)
+        sbm = bmesh.new()
+        sbm.from_mesh(mesh)
+        sbm.verts.ensure_lookup_table()
+        sbm.faces.ensure_lookup_table()
 
-        # Link object to the scene
+        # using float_color to store control points because it can store 4 values
+
+        SurfaceMesh.control_mesh_name = SurfaceMesh.control_mesh_obj.name
+        # SurfaceMesh.control_mesh_obj.hide_viewport = True
+
+        control_mesh = bpy.data.objects[SurfaceMesh.control_mesh_name]
+
+        bpy.ops.object.mode_set(mode='OBJECT')
+
+        cbm = bmesh.new()
+        cbm.from_mesh(control_mesh.data)
+        cbm.verts.ensure_lookup_table()
+
+        # undo the rotation from the original location
+
+        control_points_layer = sbm.verts.layers.float_color.new("control_points")
+        
+        for svert in sbm.verts:   
+            closest_vert_indices = []
+            # undo rotation from the original location
+            sverttemp = Vector((svert.co[0], svert.co[2], -svert.co[1]))
+            # sverttemp = Vector((svert.co[0], svert.co[1], svert.co[2]))
+            for _ in range(4):
+                min_distance = float('inf')
+                closest_vert_index = None
+                for i, cvert in enumerate(cbm.verts):
+                    if i not in closest_vert_indices:
+                        dist = (cvert.co - sverttemp).length
+                        if dist < min_distance:
+                            min_distance = dist
+                            closest_vert_index = i
+                if closest_vert_index is not None:
+                    closest_vert_indices.append(closest_vert_index)
+            svert[control_points_layer] = [closest_vert_index for closest_vert_index in closest_vert_indices]
+
+
+        cmesh = bpy.context.active_object.data
+        cbm.to_mesh(cmesh)
+        cbm.free()
+
+        # Create a new object with the mesh data
+        sbm.to_mesh(mesh)
+        sbm.free()
+        obj = bpy.data.objects.new(name="SurfaceMesh", object_data=mesh)
+
+        # Link object to the current collection
         bpy.context.collection.objects.link(obj)
+
+
+        # Set the object as active and select it
         bpy.context.view_layer.objects.active = obj
         obj.select_set(True)
 
-        obj.rotation_euler = (0, 0, 0)  # Reset rotation
-        bpy.ops.object.transform_apply(rotation=True)
-
         obj.display_type = 'WIRE'
-
+        SurfaceMesh.wireframe_mesh_created = True
         # Optionally, clear the list after creating the mesh
         # PolyhedralSplines.wireframe_vertices.clear()
-        # PolyhedralSplines.wireframe_edges.clear()
+        # SurfaceMesh.mesh_modification(300, Vector((2, 2, 2)))
 
-        print("Wireframe mesh created and updated.")
+# bpy.types.WindowManager.previous_vertex_idx = bpy.props.IntProperty(name="Previous Vertex Index", default=-1)
+persistent_data = {"prev_idx":-1, "prev_loc": None, "delta_loc": Vector((0, 0, 0,)), "delta_sum": Vector((0, 0, 0)), "suppress_handler": False}
+
+@persistent
+def edit_object_change_handler(scene, context):
+    if persistent_data["suppress_handler"]:
+        return
+    obj = bpy.context.active_object
+
+    if scene.previous_object:
+        prev_obj = scene.previous_object
+    else:
+        prev_obj = None
+
+    # Update the previously selected object
+    scene.previous_object = obj
+
+    if obj is None:
+        return None
+
+    # if obj.mode == 'OBJECT' and Mode.prev == 'EDIT' and obj.type == 'MESH':
+    if bpy.context.scene.polyhedral_splines_finished and SurfaceMesh.wireframe_mesh_created and obj.type == 'MESH' and obj.name == "SurfaceMesh":
+        if obj.mode == 'EDIT':
+            surface_mesh_obj = bpy.context.active_object
+            bm = bmesh.from_edit_mesh(obj.data)
+            bm.verts.ensure_lookup_table()
+            bm.faces.ensure_lookup_table()
+
+            control_points_layer = bm.verts.layers.float_color["control_points"]
+
+            for v in bm.verts:
+                if v.select:
+                    if persistent_data["prev_loc"] is None:
+                        print("none\n")
+                        persistent_data["prev_loc"] = v.co.copy()
+                        persistent_data["prev_idx"] = v.index
+                        persistent_data["selected"] = True
+                    elif persistent_data["prev_idx"] != v.index:
+                        print("not vertex\n")
+                        persistent_data["prev_loc"] = v.co.copy()
+                        persistent_data["delta_loc"] = Vector((0, 0, 0))
+                        persistent_data["prev_idx"] = v.index
+                    elif persistent_data["prev_idx"] == v.index:
+                        print("vertex\n")
+                        persistent_data["delta_loc"] = SurfaceMesh.delta_location(original_location=persistent_data["prev_loc"], new_location=v.co)
+                        persistent_data["delta_loc"] = Vector((persistent_data["delta_loc"][0], persistent_data["delta_loc"][2], -persistent_data["delta_loc"][1]))
+                        persistent_data["prev_loc"] = v.co.copy()
+                        persistent_data["prev_idx"] = v.index
+
+                        persistent_data["delta_sum"] += persistent_data["delta_loc"]
+                        control_points = v[control_points_layer].copy()
+                        # print(f"delta sum (total change being applied): {persistent_data['delta_sum']}")
+                        print(f"Control points to be modified: {control_points}")
+                        for control_point_index in control_points:
+                            print(f"control point getting modified: {int(control_point_index)}")
+                            persistent_data["suppress_handler"] = True
+                            SurfaceMesh.mesh_modification(int(control_point_index), persistent_data["delta_sum"])
+                            persistent_data["suppress_handler"] = False
+                        persistent_data["delta_loc"] = Vector((0, 0, 0))
+                        persistent_data["delta_sum"] = Vector((0, 0, 0))
+                        break
+
+
+
+            '''delta_mag = sum(abs(coord) for coord in persistent_data["delta_loc"])
+            if delta_mag != 0:
+                persistent_data["delta_sum"] += persistent_data["delta_loc"]
+            elif delta_mag == 0 and persistent_data["delta_sum"] != Vector((0, 0, 0)):
+                control_points = v[control_points_layer].copy()
+                # print(f"delta sum (total change being applied): {persistent_data['delta_sum']}")
+                print(f"Control points to be modified: {control_points}")
+                for control_point_index in control_points:
+                    print(f"control point getting modified: {int(control_point_index)}")
+                    persistent_data["suppress_handler"] = True
+                    SurfaceMesh.mesh_modification(int(control_point_index), persistent_data["delta_sum"])
+                    persistent_data["suppress_handler"] = False
+                persistent_data["delta_loc"] = Vector((0, 0, 0))
+                persistent_data["delta_sum"] = Vector((0, 0, 0))'''
+
+bpy.app.handlers.depsgraph_update_post.append(edit_object_change_handler)
